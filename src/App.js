f25c997ae7688bdc7bf3c709ec2d9b3ecfcf8d2c
Arton Mirakaj <artonmirakaj@Arton.lan>
2021-12-17 10:43:56 -0500

    customizing input

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ function App() {
   const [isPlaying, setIsPlaying] = useState(false);
   const [songInfo, setSongInfo] = useState({
     currentTime: 0,
-    currentDuration: 0
+    duration: 0,
+    animationPercentage: 0
   });
   const [libraryStatus, setLibraryStatus] = useState(false)
   const audioRef = useRef(null);
@@ -20,7 +21,16 @@ function App() {
   const timeUpdateHandler = (e) => {
     const current = e.target.currentTime;
     const duration = e.target.duration;
-    setSongInfo({...songInfo, currentTime: current, currentDuration: duration })
+    const roundedCurrent = Math.round(current);
+    const roundedDuration = Math.round(duration);
+    const animation = Math.round((roundedCurrent / roundedDuration) * 100)
+
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+      animationPercentage: animation
+    })
   }
 
   return (
